859b48f6fed44e21bb8d25802192b3b0b316f58e
Ingrid Lebrão <ingridlebrao@gmail.com>
2022-07-08 10:22:56 -0300

    Search styling

--- moviedb/src/pages/Search.jsx
+++ moviedb/src/pages/Search.jsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import MovieCard from '../components/MovieCard';
+import './MovieGrid.css';
+
+const searchURL = import.meta.env.VITE_SEARCH;
+const apiKEY = import.meta.env.VITE_API_KEY;
+
 const Search = () => {
-  return <div>search</div>;
+  const [searchParams] = useSearchParams();
+  const [movies, setMovies] = useState([]);
+  const query = searchParams.get('q');
+
+  const getSearch = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setMovies(data.results);
+  };
+
+  useEffect(() => {
+    const searchQuery = `${searchURL}?${apiKEY}&query=${query}`;
+    getSearch(searchQuery);
+  }, [query]);
+
+  return (
+    <div className="container">
+      <h2 className="title">
+        Resultados para <span className="query-search">{query}</span>
+      </h2>
+      <div className="movies-container">
+        {movies.length > 0 &&
+          movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
