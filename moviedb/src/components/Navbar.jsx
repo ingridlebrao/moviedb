934ff81c55e60fd3e416c670273caaed9243cd45
Ingrid Lebrão <ingridlebrao@gmail.com>
2022-07-08 10:22:10 -0300

    Updating navbar

--- moviedb/src/components/Navbar.jsx
+++ moviedb/src/components/Navbar.jsx
@@ -1,17 +1,37 @@
+import { useState } from 'react';
 import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import './Navbar.css';
 
 const Navbar = () => {
+  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(search);
+
+    if (!search) return;
+
+    navigate(`/search?q=${search}`, { replace: true });
+    setSearch('');
+  };
+
   return (
     <nav id="navbar">
       <h2>
         <Link to="/">
-          <BiCameraMovie />
+          <BiCameraMovie className="camera" />
           movieDB
         </Link>
       </h2>
-      <form>
-        <input type="text" placeholder="busque um filme" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="busque um filme"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        />
         <button type="submit">
           <BiSearchAlt2 />
         </button>
